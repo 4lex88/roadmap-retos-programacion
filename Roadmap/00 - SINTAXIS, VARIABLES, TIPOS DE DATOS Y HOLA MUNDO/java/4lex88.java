7a8023ce27f11d8284ced073159ae7e8cb4eb72b
Alex <96919617+4lex88@users.noreply.github.com>
2024-06-21 17:56:46 -0500

    Create 4lex88.java
    #00-java

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/java/4lex88.java
@@ -0,0 +1,41 @@
+package semana_00;
+
+public class Semana_00 {
+	public static void main (String[] args ) {
+		//1 - Crea un comentario en el cï¿½digo y coloca la URL del sitio web oficial del
+		 //   lenguaje de programaciï¿½n que has seleccionado.
+			//url oficial de java https://www.oracle.com/java/
+			
+			
+		//2- Representa las diferentes sintaxis que existen de crear comentarios
+		//   en el lenguaje (en una lï¿½nea, varias...).
+		//comentario en una linea
+			
+			/*
+			 * comentario en varias lineas
+			 * en java
+			 */
+			
+		//VARIABLE Y CONSTANTE
+			String miLenguaje="Java";
+			
+			final int MINUTOS=59;
+			
+		//VARIABLES DE TODOS LOS TIPOS	
+			String nombre="Alexander";
+			
+			int edad= 29;
+			
+			double peso= 75.8;
+			
+			boolean soltero= true;
+			
+			boolean hijos= false;
+			
+		//IMPRIMIR SALUDO
+			System.out.println("ï¿½Hola, "+miLenguaje+"!");
+		
+	}
+
+
+}
